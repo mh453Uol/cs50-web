a2c737dad043472af323d2eb2de8bf5548288cd1
mh453Uol <mh453@student.le.ac.uk>
2020-03-27 17:34:53 +0000

    wired up register to db

--- projects/project1/application.py
+++ projects/project1/application.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import Flask, session, request, render_template
+from flask import Flask, session, request, render_template, redirect, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+from passlib.hash import pbkdf2_sha256
 
 from .models.user import User
 
@@ -34,16 +36,41 @@ def register():
 
     if request.method == "POST":
         email = request.form.get("emailInput")
+        firstname = request.form.get("firstnameInput")
+        lastname = request.form.get("lastnameInput")
         password = request.form.get("passwordInput")
         confirm_password = request.form.get("confirmPasswordInput")
 
-        user = User(email, password, confirm_password)
+        user = User(email, firstname, lastname, password, confirm_password)
 
-        model = user.isValid()
+        if not user.is_valid():
+            model = user.model_state()
+            return render_template("register.html", model_state=model)
 
-        model["validated"] = True
-    
-        return render_template("register.html", model_state=model)
+        # 1. Email should not exist
+        # 2. Hash password
+
+        users = db.execute(
+            'SELECT * FROM "user" WHERE email = :email', {"email": email})
+
+        if users.rowcount > 0:
+            model = user.model_state()
+            model['email']['valid'] = False
+
+            return render_template("register.html", model_state=model)
+        else:
+            # Hash the password using SHA256
+            hashed_password = pbkdf2_sha256.hash(password)
+
+            # User does not exist add them to db
+            db.execute('''INSERT INTO "user" (id, email, password, firstname, lastname) 
+                VALUES (:id, :email, :password, :firstname, :lastname)''',
+                       {"id": user.id, "email": email, "password": hashed_password,
+                        "firstname": firstname, "lastname": lastname})
+
+            db.commit()
+
+            return redirect(url_for('index'))
 
 
 @app.route("/")
--- projects/project1/models/user.py
+++ projects/project1/models/user.py
@@ -1,21 +1,35 @@
 from ..utils.email_util import EmailUtil
+import uuid
 
 class User:
-
-    def __init__(self, email, password, confirm_password):
+    
+    def __init__(self, email, firstname, lastname, password, confirm_password):
+        self.id = uuid.uuid4()
         self.email = email
+        self.firstname = firstname
+        self.lastname = lastname
         self.password = password
         self.confirm_password = confirm_password
-    
-    def isValid(self):
+
+    def model_state(self):
         return {
             "email": {
                 "value": self.email,
-                "valid" : self.email and not EmailUtil().isInvalidEmail(self.email),
+                "valid": self.email and not EmailUtil().isInvalidEmail(self.email),
                 "validation_message": "Please provide a valid password."
             },
+            "firstname": {
+                "value": self.firstname,
+                "valid": self.firstname is not '',
+                "validation_message": "Please provide a firstname."
+            },
+            "lastname": {
+                "value": self.lastname,
+                "valid": self.lastname is not '',
+                "validation_message": "Please provide a lastname."
+            },
             "password": {
-                "valid": self.password != "",
+                "valid": self.password is not '',
                 "validation_message": "Please provide a valid and matching password."
             },
             "confirm_password": {
@@ -23,3 +37,12 @@ class User:
                 "validation_message": "Please provide a valid and matching password."
             }
         }
+
+    def is_valid(self):
+        dictionary = self.model_state()
+
+        for key in dictionary:
+            if dictionary.get(key)['valid'] is False:
+                return False
+
+        return True
