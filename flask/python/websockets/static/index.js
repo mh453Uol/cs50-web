a729fc0dec6f87e6b766ef1c90287eccd6305077
mh453Uol <mh453@student.le.ac.uk>
2020-04-19 09:00:15 +0100

    making spa using history api and ajax

--- flask/python/websockets/static/index.js
+++ flask/python/websockets/static/index.js
@@ -5,17 +5,37 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.nav-link').forEach((link) => {
         link.onclick = (clickEvent) => {
             // Stop link from causing a page reload
+            const page = link.dataset.page;
             clickEvent.preventDefault();
-            load_page(link.dataset.page);
+            load_page(page);
         }
     })
+
+    // User goes back or forward
+    window.onpopstate = (e) => {
+        const data = e.state;
+        updateBodyContainer(data.title, data.body);
+        
+    }
 
     function load_page(name) {
         fetch(`/${name}`)
             .then(body => body.text())
             .then(body => {
                 console.log(body);
-                document.querySelector('#body').innerHTML = body;
+                updateBodyContainer(name, body);
+                history.pushState({
+                    'title': name,
+                    'body': body
+                }, name, name);
             });
+    }
+
+    function updateBodyContainer(title, body) {
+        document.querySelector('#body').innerHTML = body;
+
+        // History Api (Update url so users are able to go back)
+        // Add tab name
+        document.title = title;
     }
 });
