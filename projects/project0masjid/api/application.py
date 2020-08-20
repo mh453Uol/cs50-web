384dc9d1398b879d802882ec51eb0de799b372be
mh453Uol <mh453@student.le.ac.uk>
2020-08-20 07:38:09 +0100

    add api endpoint for daily and jamaat prayers

---
+++ projects/project0masjid/api/application.py
@@ -0,0 +1,91 @@
+import os
+import requests
+
+from flask import Flask, request
+from datetime import datetime
+import dateutil.parser
+
+app = Flask(__name__)
+
+if not os.getenv("AIRMYPRAYER_API"):
+    raise RuntimeError("AIRMYPRAYER Api Url Not Set")
+
+base_api_url = os.getenv("AIRMYPRAYER_API")
+
+# /api/v1/<tenant>/prayers/daily?date=<iso-8601>
+@app.route("/api/v1/<tenant>/prayers/daily", methods=["GET"])
+def daily(tenant):
+    date = request.args.get("date")
+
+    if date is None:
+        date = datetime.now()
+    else:
+        date = dateutil.parser.parse(date)
+
+    response = get_daily_prayers(tenant, date)
+
+    if not response.ok:
+        return {
+            "code": f"{response}.status_code",
+            "message": f"{response}.text"
+        }
+
+    return response.json()
+
+# /api/v1/prayers/jamaat
+@app.route("/api/v1/<tenant>/prayers/jamaat", methods=["GET"])
+def jamaat(tenant):
+    response = get_jamaat_times(tenant)
+
+    if not response.ok:
+        return {
+            "code": f"{response}.status_code",
+            "message": f"{response}.text"
+        }
+
+    return response.json()
+
+
+# Make backend api request to 
+# https://prayertimes.airmyprayer.co.uk/<tenant>/prayer<tenant>?handler=getprayertimesdaily
+# Body: { date: str, month: str, establishid: str }
+def get_daily_prayers(tenant, date):
+    url = api_url(tenant, "getprayertimesdaily")
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+
+    #https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
+
+    body = {
+        "date": f"{date.day}",
+        "month": f"{date.strftime('%B')}",
+        "establishid": tenant
+    }
+
+    response = requests.post(url=url, headers=headers, json=body)
+
+    return response
+
+# https://prayertimes.airmyprayer.co.uk/<tenant>/prayer<tenant>?handler=getprayertimesjamaat
+# Body: { establishid : str }
+def get_jamaat_times(tenant):
+    url = api_url(tenant, "getprayertimesjamaat")
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    }
+
+    body = {"establishid": tenant}
+
+    response = requests.post(url=url, headers=headers, json=body)
+
+    return response
+
+
+def api_url(tenant, handler):
+    print(f"{base_api_url}/{tenant}/prayer{tenant}?handler={handler}")
+    return f"{base_api_url}/{tenant}/prayer{tenant}?handler={handler}"
