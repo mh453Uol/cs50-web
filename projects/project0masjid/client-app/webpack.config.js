9c9f9cd7e11dd6f6efccf933448430f0b995634a
mh453Uol <mh453@student.le.ac.uk>
2020-08-31 07:04:25 +0100

    use MiniCssExtractPlugin to extract css to separate file

--- projects/project0masjid/client-app/webpack.config.js
+++ projects/project0masjid/client-app/webpack.config.js
@@ -1,36 +1,47 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-var config = {
-  entry: './src/js/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/html/index.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/html/prayertimes.html',
-    })
-  ],
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-      rules: [
-          // .css files are merged into <style></style> tag
-          {
-              test: /\.css$/,
-              use: [
-                  'style-loader',
-                  'css-loader'
-              ]
-          }
-      ]
-  }
-};
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
 
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
+  const devMode = argv.mode === 'development';
+
+  const config = {
+    entry: './src/js/index.js',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/html/index.html',
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/html/prayertimes.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      })
+    ],
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [{
+        test: /\.css$/,
+        use: [{
+            // Extract stylev2.css into separate file and automatically add <link href="main.css" rel="stylesheet">
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+
+              // MiniCssExtractPlugin use hot module replacement when in development
+              hmr: devMode,
+            }
+          },
+          'css-loader',
+        ]
+      }]
+    }
+  };
+
+  if (devMode) {
     config.devtool = 'source-map';
   }
 
