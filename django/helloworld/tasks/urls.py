9ad961e5d5ab8ccd75b81627def19eafff5466e4
mh453Uol <mh453@student.le.ac.uk>
2020-09-13 11:41:20 +0100

    handling form data (edit, delete)

--- django/helloworld/tasks/urls.py
+++ django/helloworld/tasks/urls.py
@@ -6,5 +6,8 @@ app_name = "tasks"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("todo", views.add_todo, name="todo")
-]
+    path("add/todo", views.add_todo, name="add_todo"),
+    path("edit/todo/<uuid:id>", views.edit_todo, name="edit_todo"),
+    path("delete/todo/<uuid:id>", views.delete_todo, name="delete_todo")
+
+] 
--- django/helloworld/tasks/views.py
+++ django/helloworld/tasks/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
+import uuid
 
 # Create your views here.
-todos = ["foo", "bar", "baz"];
+todos = [
+    { "id": uuid.uuid4(), "name": "foo" },
+    { "id": uuid.uuid4(), "name": "bar"},
+    { "id": uuid.uuid4(), "name": "baz"}
+]
 
 def index(request):
     return render(request, "tasks/index.html", { 
@@ -11,4 +16,31 @@ def index(request):
 
 @require_http_methods(["POST"])
 def add_todo(request):
-    return request
+    form = request.POST.dict();
+
+    todos.append(
+        { "id": uuid.uuid4(), "name": form.get("todo") }
+    )
+
+    return redirect("tasks:index")
+
+def edit_todo(request, id):
+    todo = next((todo for todo in todos if todo['id'] == id), None)
+    
+    if request.method == 'GET':
+        if todo is not None:
+            return render(request, "tasks/edit.html", {"todo": todo})
+        return redirect("tasks:index")
+
+    if request.method == 'POST':
+        if todo is not None:
+            form = request.POST.dict();
+            todo['name'] = form.get("todo")
+        return redirect("tasks:index")
+
+
+def delete_todo(request, id):
+    todo = next((todo for todo in todos if todo['id'] == id), None)
+    if todo is not None:
+        return render(request, "tasks/delete.html", {"todo": todo})
+    return redirect("tasks:index")
