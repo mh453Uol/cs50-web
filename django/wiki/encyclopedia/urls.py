4177cd6964da791d5a3f6c63d868b5c551f1505e
mh453Uol <mh453@student.le.ac.uk>
2020-09-14 08:23:18 +0100

    implement search, details page

--- django/wiki/encyclopedia/urls.py
+++ django/wiki/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:wiki_title>", views.details, name="queryparams")
+    path("wiki/<str:wiki_title>", views.details, name="wiki_details"),
+    path("search", views.search, name="search")
 ]
--- django/wiki/encyclopedia/views.py
+++ django/wiki/encyclopedia/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, Http404
+from django.shortcuts import render, Http404, redirect
+from markdown2 import Markdown
 
 from . import util
 
+markdown = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -13,9 +15,28 @@ def details(request, wiki_title):
 
     if exists is not None:
         return render(request, "encyclopedia/details.html", {
-            "wiki": exists,
+            "wiki": markdown.convert(exists),
             "wiki_title": wiki_title
         })
 
     raise Http404()
 
+def search(request):
+    q = request.GET.get("q").lower()
+    wikis = util.list_entries();
+    like = []
+
+    lowercased = ""
+
+    for wiki in wikis:
+        lowercased = str(wiki).casefold()
+        if q == lowercased:
+            return redirect("wiki_details", q)
+        
+        if q.lower() in str(wiki).casefold():
+            like.append(wiki)
+
+    return render(request, "encyclopedia/search-results.html", {
+        "query": q,
+        "entries": like
+    })
