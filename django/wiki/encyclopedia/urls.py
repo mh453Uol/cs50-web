2a54512e54f64d81296073a3b11975ed6fab3920
mh453Uol <mh453@student.le.ac.uk>
2020-09-19 12:34:10 +0100

    add functionality to add new wiki

--- django/wiki/encyclopedia/urls.py
+++ django/wiki/encyclopedia/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("wiki/create", views.create, name="create"),
+    path("wiki/search", views.search, name="search"),
     path("wiki/<str:wiki_title>", views.details, name="wiki_details"),
-    path("search", views.search, name="search")
 ]
--- django/wiki/encyclopedia/views.py
+++ django/wiki/encyclopedia/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render, Http404, redirect
+from django.http import request
+
 from markdown2 import Markdown
 
 from . import util
+from .form import WikiForm
 
 markdown = Markdown()
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
 
 def details(request, wiki_title):
     exists = util.get_entry(wiki_title)
@@ -21,9 +26,10 @@ def details(request, wiki_title):
 
     raise Http404()
 
+
 def search(request):
     q = request.GET.get("q").lower()
-    wikis = util.list_entries();
+    wikis = util.list_entries()
     like = []
 
     lowercased = ""
@@ -32,11 +38,37 @@ def search(request):
         lowercased = str(wiki).casefold()
         if q == lowercased:
             return redirect("wiki_details", q)
-        
+
         if q.lower() in str(wiki).casefold():
             like.append(wiki)
 
     return render(request, "encyclopedia/search-results.html", {
         "query": q,
         "entries": like
+    })
+
+
+def create(request):
+    form = None
+    
+    if request.method == "POST":
+        print(request.POST)
+
+        form = WikiForm(request.POST)
+
+        if form.is_valid():
+            
+            title = form.cleaned_data["title"]
+
+            if util.get_entry(title) is not None:
+                form.add_error("title", f"Wiki with title {title} already exists")
+            else:
+                # add document to disk
+                util.save_entry(title,form.cleaned_data["body"])
+                return redirect("wiki_details", wiki_title=title)
+    else:
+        form = WikiForm()
+
+    return render(request, "encyclopedia/create.html", {
+        "form": form
     })
