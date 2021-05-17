e03b4ec4d3d07537dfe066423a0bf17c72cd08f3
mh453Uol <mh453@student.le.ac.uk>
2021-05-17 06:22:14 +0100

    load the tenant config

--- projects/project0masjid/client-app-v2/masjidma/src/App.tsx
+++ projects/project0masjid/client-app-v2/masjidma/src/App.tsx
@@ -1,14 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+import configuration from './config/config.prod.json';
+import { getQueryString } from './util/util';
+
+interface Props { }
+interface TenantConfiguration {
+  name: string,
+  id: number,
+  displayRamadanTimes: boolean,
+  ramadanStart: string,
+  ramadanEnd: string,
+  ramadanTimetable: string,
+  announcements: {
+    message: string,
+    from: string,
+    to: string
+  }[]
+}
+interface Configuration {
+  tenants: TenantConfiguration[]
+}
+interface State {
+  date: Date,
+  configuration: Configuration,
+  tenant?: TenantConfiguration
+}
+
+class App extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    
+    this.state = {
+      date: new Date(),
+      configuration: configuration,
+    }
+
+    //console.log(configuration);
+  }
+
+  getSelectedTenant(): TenantConfiguration {
+    const tenantId = window.localStorage.getItem("tenant");
+    const defaulTenant = this.state.configuration.tenants[0];
+
+    if (tenantId === null) {
+      return defaulTenant;
+    }
+
+    const tenant = this.state.configuration.tenants.find(t => t.id === Number(tenantId));
+
+    if (!tenant) {
+      return defaulTenant;
+    }
+
+    return tenant;
+  }
+
+  initalizeTenant(): void {
+    // look at the url since we should have a tenant parameter e.g. xyz.com?tenant=1
+    let tenantId = getQueryString('tenant', window.location.href);
+  
+    // if tenant not specified look at localStorage else default back
+    if (!tenantId) {
+      const tenant = window.localStorage.getItem('tenant');
+
+      if (tenant) {
+        tenantId = tenant;
+      } else {
+        tenantId = this.state.configuration.tenants[0].id.toString();
+      }
+    }
+
+    window.localStorage.setItem('tenant', tenantId);
+  }
+
+  componentDidMount() {
+    this.initalizeTenant();
+
+    const tenant = this.getSelectedTenant();
+
+    this.setState({
+      tenant: tenant,
+      configuration: this.state.configuration,
+      date: this.state.date
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <span>{JSON.stringify(this.state.configuration)}</span>
+        <div>date: {this.state.date.toString()}</div>
+        <div>tenant: {JSON.stringify(this.state.tenant)}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ projects/project0masjid/client-app-v2/masjidma/src/util/util.ts
@@ -0,0 +1,12 @@
+/**
+ * Get the value of a querystring
+ * @param  {String} field The field to get the value of
+ * @param  {String} url   The URL to get the value from (optional)
+ * @return {String}       The field value
+ */
+ export const getQueryString = (field: string, url: string) => {
+    var href = url ? url : window.location.href;
+    var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+    var string = reg.exec(href);
+    return string ? string[1] : null;
+};
