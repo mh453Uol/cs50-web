47301a55b1c2dd93b792c557566a8b96c4c20a4c
mh453Uol <mh453@student.le.ac.uk>
2021-05-17 07:03:47 +0100

    create custom navigation component

---
+++ projects/project0masjid/client-app-v2/masjidma/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,54 @@
+import React, { ReactNode } from 'react';
+import { Tenant } from '../../models/Tenant';
+
+interface Props {
+  children: ReactNode,
+  tenants: Tenant[]
+}
+
+const Navigation: React.FC<Props> = (props) => (
+  <div data-testid="Navigation">
+
+    {/* Enable parent component to project content */}
+    {props.children}
+    
+    <nav className="navbar fixed-bottom navbar-dark bg-dark">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+        aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" href="./index.html">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="./ramadan.html">Ramadan 2021</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="./about.html">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="./contact-us.html">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+
+      <a className="navbar-brand ellipsis establishment-name" href="#"></a>
+      <div className="btn-group dropup">
+        <button className="btn btn-primary btn-sm dropdown-toggle establishment-dropdown" type="button"
+          id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <span>🕌</span>
+        </button>
+        <div className="dropdown-menu dropdown-menu-right" id="setting-dropdown" aria-labelledby="dropdownMenuButton">
+          <div id="tenant-dropdown">
+          </div>
+        </div>
+      </div>
+    </nav>
+
+  </div>
+);
+
+export default Navigation;
