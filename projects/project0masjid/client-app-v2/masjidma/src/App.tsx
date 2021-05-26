85c147978e2ea9a7e90b6920c5ce79eb88a6379d
mh453Uol <mh453@student.le.ac.uk>
2021-05-26 10:37:05 +0100

    encapulate bookmark instruction

--- projects/project0masjid/client-app-v2/masjidma/src/App.tsx
+++ projects/project0masjid/client-app-v2/masjidma/src/App.tsx
@@ -13,6 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { JamaatTime } from './models/JamaatTime';
 import { PrayerTime } from './models/PrayerTime';
 import Table from './components/Table/Table';
+import BookmarkInstruction from './components/BookmarkInstruction/BookmarkInstruction';
 
 interface Props { }
 interface Configuration {
@@ -79,12 +80,9 @@ class App extends React.Component<Props, State> {
   }
 
   getSalahTime(): Promise<[JamaatTime, PrayerTime]> {
-    
+
     this.setState({
-      tenant: this.state.tenant,
-      configuration: this.state.configuration,
-      date: this.state.date,
-      isLoading: true //set isLoading to true
+      isLoading: true
     });
 
     return Promise.all([
@@ -100,17 +98,12 @@ class App extends React.Component<Props, State> {
 
     this.setState({
       tenant: tenant,
-      configuration: this.state.configuration,
-      date: this.state.date
     })
 
     this.getSalahTime()
       .then(([jamaat, startTime]) => {
-        
+
         this.setState({
-          tenant: tenant,
-          configuration: this.state.configuration,
-          date: this.state.date,
           isLoading: false,
           salah: {
             jamaat: jamaat,
@@ -123,8 +116,6 @@ class App extends React.Component<Props, State> {
   tenantSelected(newTenant: Tenant): void {
     this.setState({
       tenant: newTenant,
-      configuration: this.state.configuration,
-      date: this.state.date
     })
   }
 
@@ -134,48 +125,20 @@ class App extends React.Component<Props, State> {
         tenants={this.state.configuration.tenants}
         selectedTenant={this.state.tenant}
         tenantSelected={this.tenantSelected}>
-        <Header
-          date={this.state.date}
-          isLoading={this.state.isLoading}
-          salah={this.state.salah}>
-        </Header>
-        {/* <div className="App">
-          <div>{JSON.stringify(this.state.salah)}</div>
-          <div>date: {this.state.date.toString()}</div>
-          <div>tenant: {JSON.stringify(this.state.tenant)}</div>
-        </div> */}
+        <div id="App">
+          <Header
+            date={this.state.date}
+            isLoading={this.state.isLoading}
+            salah={this.state.salah}>
+          </Header>
 
-      <div id="App">
-        <Table
-          salah={this.state.salah}>
-        </Table>
+          <Table
+            salah={this.state.salah}>
+          </Table>
 
-        <div className="m-2">
-            <p className="text-center">
-                <b>Follow these instructions to add this page to your phones home screen</b>
-            </p>
-
-            <div className="text-center">
-                <b>iPhone</b>
-            </div>
-
-            <ol>
-                <li>Tap the Share icon</li>
-                <li>Tap add to Home Screen</li>
-                <li>Enter name for bookmark</li>
-            </ol>
-            
-            <div className="text-center">
-                <b>Android</b>
-            </div>
-
-            <ol>
-                <li>Tap the menu icon (3 dots in upper right-hand corner)</li>
-                <li>Tap add to homescreen</li>
-                <li>Enter name for bookmark</li>
-            </ol>
+          <BookmarkInstruction></BookmarkInstruction>
+          
         </div>
-      </div>
       </Navigation>
     );
   }
