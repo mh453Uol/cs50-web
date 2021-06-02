84a55b0ce906f1e849b352322a5ab74020ae232d
mh453Uol <mh453@student.le.ac.uk>
2021-06-02 16:28:45 +0100

    polyfill ie9

--- projects/project0masjid/client-app-v2/masjidma/src/index.tsx
+++ projects/project0masjid/client-app-v2/masjidma/src/index.tsx
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import 'react-app-polyfill/ie9';
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
