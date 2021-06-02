6f017affacafd1ea1d3febd432fc6393b86c3fe7
mh453Uol <mh453@student.le.ac.uk>
2021-06-02 16:42:07 +0100

    add polyfill for .find

--- projects/project0masjid/client-app-v2/masjidma/src/index.tsx
+++ projects/project0masjid/client-app-v2/masjidma/src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
 
 ReactDOM.render(
   <React.StrictMode>
