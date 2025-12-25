c53be9f6b5b2db15c2ac057b6a9a653cb728a353
mh453Uol <hussainmajid95@gmail.com>
2025-12-25 15:00:47 +0000

    feat: improve nav bar

--- projects/project0masjid/client-app-v2/masjidma/src/components/Navigation/Navigation.tsx
+++ projects/project0masjid/client-app-v2/masjidma/src/components/Navigation/Navigation.tsx
@@ -1,9 +1,9 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { Tenant } from '../../models/Tenant';
 
 import './Navigation.css';
 
-import { Dropdown, DropdownButton, Nav, Navbar } from 'react-bootstrap';
+import { Container, Dropdown, DropdownButton, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 
@@ -14,7 +14,7 @@ interface Props {
   onTenantSelected: (tenantId: number) => void
 }
 
-const onSetTenant = (e: any, tenantSelectedFn: (tenantId:number) => void) => {
+const onSetTenant = (e: any, tenantSelectedFn: (tenantId: number) => void) => {
   const tenantId = e?.target?.dataset?.tenant;
 
   if (tenantId) {
@@ -24,37 +24,38 @@ const onSetTenant = (e: any, tenantSelectedFn: (tenantId:number) => void) => {
 };
 
 const Navigation = (props: Props) => {
-
   return (
-    <div data-testid="Navigation">
-
-      {/* Enable parent component to project content */}
+    <div data-testid="Navigation">      {/* Enable parent component to project content */}
       <div className="app-container">
         {props.children}
       </div>
-
-  
-      <Navbar collapseOnSelect={true} fixed="bottom" bg="light" variant="light" expand={false} style={{ flexWrap: 'nowrap'}}>
+      <Navbar collapseOnSelect={true} fixed="bottom" expand={false} data-bs-theme="dark" bg='dark'>
+        <Container>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto ml-2 text-center">
               <Nav.Link as={Link} to="/" className="nav-link" eventKey="1" key={1}>Home</Nav.Link>
               <Nav.Link as={Link} to={`/radio/${props?.tenant?.id}?utm_source=southcourtmosquedotlive&utm_medium=navbar-home-page`} className="nav-link" eventKey="2" key={2}>Radio</Nav.Link>
-
               {props?.tenant?.displayRamadanTimes &&
                 <Nav.Link as={Link} to="/ramadan" className="nav-link" eventKey="3" key={3}>Ramadan {new Date().getFullYear()}</Nav.Link>
               }
-
               {props?.tenant?.donationLink &&
                 <Nav.Link href={props?.tenant?.donationLink} className="nav-link" eventKey={6} key={6}>Donate</Nav.Link>
               }
-
               <Nav.Link as={Link} to="/about" className="nav-link" eventKey="4" key={4}>About</Nav.Link>
               <Nav.Link as={Link} to="/contact-us" className="nav-link" eventKey="5" key={5}>Contact Us</Nav.Link>
             </Nav>
+            <div className="card my-2 ml-3">
+              <div className="card-body d-flex align-items-center justify-content-center">
+                <img src="https://wedesignedit.co.uk/images/favicon/favicon-32x32.png" alt="wedesignedit logo" className="me-3" />
+                <div>
+                  <p className="mb-0">Developed By <a href="https://wedesignedit.co.uk/" target="_blank" rel="noopener noreferrer">WeDesignedIt</a></p>
+                </div>
+              </div>
+            </div>
           </Navbar.Collapse>
 
-          <DropdownButton menuAlign="right" className="establishment-dropdown" drop="up" title={props.tenant?.name || 'Loading'}>
+          <DropdownButton className="establishment-dropdown" drop="up" title={props.tenant?.name || 'Loading'}>
             {props?.tenants?.map(tenant =>
               <Dropdown.Item
                 className={classNames({ "active": tenant.id === props?.tenant?.id, "ellipsis": true })}
@@ -65,6 +66,7 @@ const Navigation = (props: Props) => {
               </Dropdown.Item>
             )}
           </DropdownButton>
+        </Container>
       </Navbar>
     </div>
   );
